c11fb90461385257e405af8bff67d8e8d1ed3f88
@StevieAmb <s.ambroise@protonmail.com>
2025-01-12 15:43:13 -0700

    Hide old answer and math dialogue

--- main.js
+++ main.js
@@ -59,7 +59,8 @@ const getMathProblemAnswer = () => {
 
 const restartGame = () => {
   startMathProblemRound()
-  show([userAnswerInput, userMathProblemDialogue])
+  show([userAnswerInput])
+  hide([userMathProblemDialogue, userResponse])
 }
 
 
